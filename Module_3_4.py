3366d4ab9c03ed30ea72cf56b60224878ca06420
SyutkinMO <syutkin.mo@gmail.com>
2024-11-13 16:03:12 +0500

    beat1

--- Module_3_4.py
+++ Module_3_4.py
@@ -1,4 +1,14 @@
-def single_root_words(root_word, *other_words):
+# -----------------Произвольное число параметров-----------------
+
+
+"""Напишите функцию single_root_words, которая принимает одно обязательное слово в параметр root_word, а далее
+неограниченную последовательность в параметр *other_words.
+Функция должна составить новый список same_words только из тех слов списка other_words,
+которые содержат root_word или наоборот root_word содержит одно из этих слов.
+После вернуть список same_words в качестве результата своей работы."""
+
+
+def single_root_words(root_word, *other_words) -> list:
     same_words = []
     up_other = list(s.upper() for s in other_words)
     up_root = root_word.upper()
--- module_3_5.py
+++ module_3_5.py
@@ -1,6 +1,13 @@
+# ---------------Рекурсия---------------
+
+
+"""Напиши функцию get_multiplied_digits, которая принимает аргумент целое число number и
+подсчитывает произведение цифр этого числа."""
+
+
 def get_multiplied_digits(number):
-    str_number = str(number)
-    first = int(str_number[0])
+    str_number = str(number)  # строковое представление(str) числа number
+    first = int(str_number[0])  # первый символ из str_number в числовом представлении(int).
     if len(str_number) == 1:
         return first
     else:
